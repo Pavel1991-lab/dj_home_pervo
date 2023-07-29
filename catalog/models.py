d3350ed9342da2a7f5453876c3bafa37f6bd9c1e
Pavel1991-lab <sotnikov.pavel.91@mail.ru>
2023-07-29 12:24:52 +0300

    new

--- catalog/models.py
+++ catalog/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='название')  # наименование
     description = models.TextField(verbose_name='описание')  # описание
-    image = models.ImageField(upload_to='product_images')  # изображение (превью)
+    image = models.ImageField(upload_to='product_images', blank=True, null=True)  # изображение (превью)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')  # цена за покупку
     created_date = models.DateTimeField(auto_now_add=True)  # дата создания
