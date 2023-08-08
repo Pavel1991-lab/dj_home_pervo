41e6d9a8b77eb2f21344e63b98699c9f2fcd0ce6
Pavel1991-lab <sotnikov.pavel.91@mail.ru>
2023-08-08 20:39:39 +0300

    confirm

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import Contactlistview, ProductByCategoryListView, Productlistview, Productcreateview, Fitbeklistview
+from catalog.views import Contactlistview, ProductByCategoryListView, Productlistview
 
 from catalog.apps import CatalogConfig
 
@@ -12,12 +12,12 @@ urlpatterns = [
     path('', Productlistview.as_view(), name='product_list'),
     path('contacts/', Contactlistview.as_view(), name = 'contact_list'),
     path('good/<int:pk>', ProductByCategoryListView.as_view(), name='product_detail'),
-    path('fidbeck/', Fitbeklistview.as_view(), name = 'blog_list'),
-    path('create/', Productcreateview.as_view(), name='create'),
-    # path('', ..., name='list'),
-    # path('view/<int:pk>/', ..., name='view'),
-    # path('edit/<int:pk>/', ..., name='edit'),
-    # path('delete/<int:pk>/', ..., name='delete')
+    # path('fidbeck/', Fitbeklistview.as_view(), name = 'blog_list'),
+    # path('create/', Productcreateview.as_view(), name='create'),
+    # # path('', ..., name='list'),
+    # # path('view/<int:pk>/', ..., name='view'),
+    # # path('edit/<int:pk>/', ..., name='edit'),
+    # # path('delete/<int:pk>/', ..., name='delete')
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
---
+++ fidbeck/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+
+class Fidbeck(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название')
+    slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
+    content = models.TextField(verbose_name='содержимое')
+    preview = models.ImageField(upload_to='product_images', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+    views_count = models.IntegerField(default=0)
+
+
+
