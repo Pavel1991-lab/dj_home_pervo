f8cada4c2efac81d9c241725052957a638094c72
Pavel1991-lab <sotnikov.pavel.91@mail.ru>
2023-08-12 00:13:33 +0300

    apple

--- catalog/views.py
+++ catalog/views.py
@@ -27,6 +27,20 @@ class ProductCreate(CreateView):
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
+    def form_valid(self, form):
+        name = form.cleaned_data.get('name')
+        description = form.cleaned_data.get('description')
+
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+
+        for word in forbidden_words:
+            if word in name.lower() or word in description.lower():
+                form.add_error(None, f"Запрещенное слово '{word}' найдено в названии или описании продукта.")
+                return self.form_invalid(form)
+
+        return super().form_valid(form)
+
 class ProductUpdateview(UpdateView):
     model = Product
     form_class = ProductForm
